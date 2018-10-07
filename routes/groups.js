f5db1a52805d735a04635b7da4ebc0ec823ab69b
Alice Williams <aliceklwilliams@gmail.com>
2018-10-07 15:40:21 +0100

    Add back in the ability to delete groups. Use a modal to ask the user if they would like to delete associated people.

--- routes/groups.js
+++ routes/groups.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let router = express.Router();
 
+let mongoose = require("mongoose");
+
 let Person = require("../models/Person");
 let Group = require("../models/Group");
 
